ff8166da2759379fd804ea803b0ca80c8702962b
Aviral <2022sp93021@wilp.bits-pilani.ac.in>
2023-03-25 02:16:41 +0530

    added a calculator app with tests

---
+++ src/main/java/com/myapp/calculatorapp/SimpleCalculator.java
@@ -0,0 +1,106 @@
+package main.java.com.myapp.calculatorapp;
+
+import java.util.Locale;
+import java.util.Scanner;
+/*Very very simple Java Calculator for beginners just for 4 operations on 
+ * two variables without exception and while loop*/
+
+public class SimpleCalculator {
+	Scanner scan = new Scanner(System.in);
+
+	public void showMenu() {
+		System.out.println("1.Addition");
+		System.out.println("2.Subtraction");
+		System.out.println("3.Multiplication");
+		System.out.println("4.Division");
+
+	}
+
+	public String division(double a, double b) {
+		scan.useLocale(Locale.US); // set to this have "." instead of "," it depends on your region
+		// double a, b;
+		// System.out.println("Enter first value");
+		// a = scan.nextDouble();
+		// System.out.println("Enter second value");
+		// b = scan.nextDouble();
+		if (b == 0)
+			return "Division by zero not possible";
+
+		double val = a / b;
+		return String.valueOf(val);
+	}
+
+	public double multiplication(double a, double b) {
+		scan.useLocale(Locale.US);
+		// double a, b;
+		// System.out.println("Enter first value");
+		// a = scan.nextDouble();
+		// System.out.println("Enter second value");
+		// b = scan.nextDouble();
+		double val = a * b;
+
+		return val;
+	}
+
+	public double subtraction(double a, double b) {
+		scan.useLocale(Locale.US);
+		// double a, b;
+		// System.out.println("Enter first value");
+		// a = scan.nextDouble();
+		// System.out.println("Enter second value");
+		// b = scan.nextDouble();
+		double val = a - b;
+
+		return val;
+	}
+
+	public double addition(double a, double b) {
+		scan.useLocale(Locale.US);
+		// double a, b;
+		// System.out.println("Enter first value");
+		// a = scan.nextDouble();
+		// System.out.println("Enter second value");
+		// b = scan.nextDouble();
+		double val = a + b;
+
+		return val;
+	}
+
+	public static void main(String[] args) {
+		SimpleCalculator calc = new SimpleCalculator();
+		Scanner scan = new Scanner(System.in);
+		scan.useLocale(Locale.US);
+		int x;
+		double score, a, b;
+		calc.showMenu();
+		x = scan.nextInt();
+		System.out.println("Enter first value");
+		a = scan.nextDouble();
+		System.out.println("Enter second value");
+		b = scan.nextDouble();
+		switch (x) {
+			case 1:
+				score = calc.addition(a,b);
+				System.out.println(score);
+				break;
+			case 2:
+				score = calc.subtraction(a,b);
+				System.out.println(score);
+				break;
+			case 3:
+				score = calc.multiplication(a,b);
+				System.out.println(score);
+				break;
+			case 4:
+				String msg;
+				msg = calc.division(a,b);
+				System.out.println(msg);
+				break;
+			default:
+				System.out.println("Wrong choice");
+				break;
+		}
+		scan.close();
+	}
+
+}
---
+++ src/test/java/com/myapp/calculatorapp/CalculatorTest.java
@@ -0,0 +1,47 @@
+package test.java.com.myapp.calculatorapp;
+import main.java.com.myapp.calculatorapp.SimpleCalculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+public class CalculatorTest
+{
+    SimpleCalculator app = new SimpleCalculator();
+    @Test
+    public void testAddition() {
+        double a = 1, b = 1;
+        assertEquals(2.0, app.addition(a,b));
+    }
+
+    @Test
+    public void testSubtraction() {
+        // SimpleCalculator app = new SimpleCalculator();
+        double a = 1, b = 1;
+        assertEquals(0.0, app.subtraction(a,b));
+    }
+
+    @Test
+    public void testMultiplication() {
+        // SimpleCalculator app = new SimpleCalculator();
+        double a = 2, b = 1;
+        assertEquals(2.0, app.multiplication(a,b));
+    }
+
+    @Test
+    public void testDivision1() {
+        // SimpleCalculator app = new SimpleCalculator();
+        double a = 1, b = 1;
+        assertEquals("1.0", app.division(a,b));
+    }
+
+    @Test
+    public void testDivision2() {
+        // SimpleCalculator app = new SimpleCalculator();
+        double a = 1, b = 0;
+        assertEquals("Division by zero not possible", app.division(a,b));
+    }
+}
